e8689251aca5b031241b50941950350d6d274f28
Khyati Bardolia <khyati.bardolia@gmail.com>
2024-01-27 01:40:28 -0500

    chore: add dynamic gradient color based on house color, create mixin

--- src/components/HousesList/HousesList.tsx
+++ src/components/HousesList/HousesList.tsx
@@ -9,25 +9,41 @@ export type Props = {
 
 export const HousesList: FC = ({houses}: Props) => {
 
+    const getHouseClassName = (houseColor: string) => {
+        switch (houseColor.toLowerCase()) {
+            case 'scarlet and gold1':
+                return styles['scarlet_and_gold'];
+            case 'blue and bronze':
+                return styles.blue_and_bronze;
+            case 'yellow and black':
+                return styles.yellow_and_black;
+            case 'green and silver':
+                return styles.green_and_silver;
+            default:
+                return styles.default;
+        }
+    };
+
     return <div className={styles['house_list_wrapper']}>
         {houses && houses.length && houses.map((house: Houses) => {
             return <Card
-                         key={house.id}
-                         header={
-                             <div className={styles['house_list_wrapper__header']}>
-                                 <span>{house.name}</span>
-                                 <span>{house.animal}</span>
-                             </div>
-                         }
-                         content={
-                             <div className={styles['house_list_wrapper__content']}>
-                                 <div>{/*Todo: show gradient bar*/}</div>
-                                 <div className={styles['house_list_wrapper__content__founder']}>
-                                     <span>Founder: </span>
-                                     <span>{house.founder}</span>
-                                 </div>
-                             </div>
-                         }/>
+                key={house.id}
+                header={
+                    <div className={styles['house_list_wrapper__header']}>
+                        <span>{house.name}</span>
+                        <span>{house.animal}</span>
+                    </div>
+                }
+                content={
+                    <div className={styles['house_list_wrapper__content']}>
+                        <div className={`${styles['house_list_wrapper__content__house-color']} 
+                                 ${getHouseClassName(house.houseColours)}`}/>
+                        <div className={styles['house_list_wrapper__content__founder']}>
+                            <span>Founder: </span>
+                            <span>{house.founder}</span>
+                        </div>
+                    </div>
+                }/>
         })}
     </div>
 }
